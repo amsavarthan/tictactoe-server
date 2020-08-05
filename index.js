6bfd858f0045082a81ae5aa513e5fbb6c0c293ef
Amsavarthan Lv <amsavarthan.a@gmail.com>
2020-08-06 01:13:23 +0530

    🔧 Minor fix

--- index.js
+++ index.js
@@ -19,34 +19,40 @@ mongoose
     app.get("/", (req, res) => {
       res.send({ message: "I'm alive" });
     });
+
+    io.on("connection", (socket) => {
+      //on creating it emits 'created-room'
+      socket.on("create-room", ({ name, roomId }) => {
+        socketHandler.createRoom(io, socket, name, roomId);
+      });
+
+      //on joined it emits 'joined-room'
+      socket.on("join-room", ({ name, roomId, oldSocketId }) => {
+        socketHandler.joinRoom(io, socket, name, roomId, oldSocketId);
+      });
+
+      socket.on("on-user-selection", ({ roomId, completedCells, playedBy }) => {
+        socketHandler.updateGameBoard(
+          io,
+          socket,
+          roomId,
+          completedCells,
+          playedBy
+        );
+      });
+
+      socket.on("game-restart", (roomId) => {
+        socketHandler.restartGame(socket, roomId);
+      });
+
+      socket.on("disconnecting", () => {
+        socketHandler.removeOrUpdateClientStatus(io, socket);
+      });
+    });
   })
   .catch((reason) => {
     console.log(reason);
   });
-
-io.on("connection", (socket) => {
-  //on creating it emits 'created-room'
-  socket.on("create-room", ({ name, roomId }) => {
-    socketHandler.createRoom(io, socket, name, roomId);
-  });
-
-  //on joined it emits 'joined-room'
-  socket.on("join-room", ({ name, roomId, oldSocketId }) => {
-    socketHandler.joinRoom(io, socket, name, roomId, oldSocketId);
-  });
-
-  socket.on("on-user-selection", ({ roomId, completedCells, playedBy }) => {
-    socketHandler.updateGameBoard(io, socket, roomId, completedCells, playedBy);
-  });
-
-  socket.on("game-restart", (roomId) => {
-    socketHandler.restartGame(socket, roomId);
-  });
-
-  socket.on("disconnecting", () => {
-    socketHandler.removeOrUpdateClientStatus(io, socket);
-  });
-});
 
 http.listen(port, () => {
   console.log(`Server is running at port ${port} :)`);
